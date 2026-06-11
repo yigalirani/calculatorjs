b76d2e60f001cc770cff8a5c480914cce15be634
Yigal Irani <yigal@symbolclick.com>
2026-06-11 18:52:55 +0300

    calculators js

--- src/calc.ts
+++ src/calc.ts
@@ -1,33 +1,57 @@
-import {calc} from './common.js'
-export function calc_trip(){
-  const trip1=calc('tel aviv to paris with two days in athens','$',[
+import { calc } from './common.js'
+const scenarios = [{
+  title: 'tel aviv to paris with two days in athens', lines: [
     {
-      name:"Tel Aviv - Athens",
-      comment:"june 15 3:10 pm – 5:20 pm",
-      url:"https://kay.ac/eY7dea",
-      amt:111
+      name: "Tel Aviv - Athens",
+      comment: "june 15 3:10 pm – 5:20 pm",
+      url: "https://kay.ac/eY7dea",
+      amt: 111
     },
     {
-      name:"two nights in athens hostel",
-      comment:"june 15 - june 17",
-      url:"https://kay.ac/jxqhiq",
-      amt:64
+      name: "two nights in athens hostel",
+      comment: "june 15 - june 17",
+      url: "https://kay.ac/jxqhiq",
+      amt: 64
     },
-    
-   {
-      name:"Athens - Paris",
-      comment:"june 17 12:10 pm – 2:40 pm",
-      url:"https://kay.ac/imrhw6",
-      amt:78
+
+    {
+      name: "Athens - Paris",
+      comment: "june 17 12:10 pm – 2:40 pm",
+      url: "https://kay.ac/imrhw6",
+      amt: 78
     }
-  ])
-  const trip2=calc('diret flight','$',[
-       {
-      name:"direct flight with arkis",
-      comment:"june 17 10:30 am – 2:30 pm",
-      url:"https://kay.ac/F4bubc",
-      amt:463
+  ]
+}, {
+  title: 'tel aviv to paris with one days in athens',
+  lines: [
+    {
+      name: "Tel Aviv - Athens",
+      comment: "june 16 3:10 pm – 5:20 pm",
+      url: "https://kay.ac/vMD6Lq",
+      amt: 121
+    },
+    {
+      name: "one night in athens hostel",
+      comment: "june 16 - june 17",
+      url: "https://kay.ac/jxqhiq",
+      amt: 32
+    },
+    {
+      name: "Athens - Paris",
+      comment: "june 17 12:10 pm – 2:40 pm",
+      url: "https://kay.ac/imrhw6",
+      amt: 32 * 2
     }
-  ])
-  return trip1+trip2
+  ]
+}, {
+  title: 'diret flight',
+  lines: [{
+    name: "direct flight with arkis",
+    comment: "june 17 10:30 am – 2:30 pm",
+    url: "https://kay.ac/F4bubc",
+    amt: 463
+  }]
+}]
+export function calc_trip() {
+  return `${calc('$',scenarios)}version 3`
 }
--- src/common.ts
+++ src/common.ts
@@ -4,37 +4,51 @@ export interface Line{
   comment:string
   url:string
 }
+export interface Scenario{
+  title:string,
+  lines:Line[]
+}
 function make_link(url:string){
   return `<a target="_blank" rel="noopener noreferrer" href="${url}">${url} </a> `
 }
-  
-export function calc(title:string,currency:string,lines:Line[]){
+function calc_one(scenario:Scenario){
+  let ans=0
+  for (const line of scenario.lines)
+    ans+=line.amt
+  return ans
+}
+function td(a:string){
+  return `<td>${a}</td>`
+}
+function tr(...a:string[]){
+  return `<tr>${a.map(td).join('\n')}</tr>`
+  //return `<td>${a}</td>`
+}
+function calc_scenario_html(currency:string,scenario:Scenario,cheapset:Scenario){
   let total=0
-  const rows=[]
+  const {lines,title}=scenario
+  const cheapset_msg=scenario===cheapset?"-cheapset":''
+  const rows=[`<tr class=title><td colspan=4>${title} ${cheapset_msg}</td></tr>`]
   for (const {name,amt,comment,url} of lines){
     total+=amt
-    rows.push(`<tr>
-      <td>${name}</td>
-      <td>${comment}</td>
-      <td>${make_link(url)}</td>
-      <td>${currency}${amt}</td>
-    </tr>`)
+    rows.push(tr(name,comment,make_link(url),`${currency}${amt}`))
   }
-    rows.push(`<tr>
-      <td>total</td>
-      <td></td>
-      <td></td>
-      <td>${currency}${total}</td>
-    </tr>`)  
-    return `
-    <h3>${title}</h3><table>
-    <tr>
+  rows.push(tr('total','','',`${currency}${total}`))
+  return rows.join('\n')
+}
+
+export function calc(currency:string,scenarios:Scenario[]){
+  const cheapset=scenarios.reduce((a, b) =>calc_one(b) < calc_one(a) ? b : a)
+  const html=[`<tr>
       <th>item</th>
       <th>comment</th>
       <th>link</th>
       <th>amt</td>
-    </tr>
-    ${rows.join('\n')}
-    </table>
-    `
+    </tr>`]
+  for (const scenario of scenarios){
+    const one_html=calc_scenario_html(currency,scenario,cheapset)
+    html.push(one_html)
+  }
+  const ans= `<table>${html.join('\n')}</table>`
+  return ans
 }
